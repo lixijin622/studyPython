16d968a809028d19997d61271c3faad78b29d0ca
x <lixijin622@foxmail.com>
2016-04-08 17:24:27 +0800

    ...

---
+++ def-deep.py
@@ -0,0 +1,15 @@
+def ask_ok(prompt, retries=4, complaint='Yes or no, please!'):
+    while True:
+        ok = input(prompt)
+        # print(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries -= 1  # retries = retries - 1
+        print(retries)
+        if retries < 0:
+            raise OSError('uncooperative user')
+        print(complaint)
+
+ask_ok("Do you really want to quit?", 1)
